2baa5d56d65c7ca65d93b25bd7a2221d84ef156d
Jason Vo <102493073+voxuanthang203@users.noreply.github.com>
2023-07-16 19:43:44 -0400

    Update shortestPath.cpp

---
+++ shortestPath.cpp
@@ -0,0 +1,15 @@
+#include "shortestPath.h"
+
+vector<vector<cell>> shortestPath(const Matrix *matrix) {
+    //create a new matrix for the output (same size with input matrix)
+    vector<vector<cell>> output(matrix->rows, vector<cell>(matrix->cols));
+
+    //initialize first row with the same value of the input matrix
+    for (int i = 0; i < matrix->cols; i++) {
+        output[0][i].length = matrix->matrix[0][i];
+        output[0][i].start_cell = i;
+        output[0][i].path = "";
+    }
+
+    return output;
+}
