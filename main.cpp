fb4806a065e62b352e8e905e590f0ca161515d4b
Jason Vo <102493073+voxuanthang203@users.noreply.github.com>
2023-07-16 16:33:48 -0400

    Delete print matrix from main

--- main.cpp
+++ main.cpp
@@ -11,11 +11,4 @@ int main() {
     string input = "input-small.txt";
 
     Matrix* matrix = createMatrix(input);
-
-    for (int i = 0; i < matrix->rows; ++i) {
-        for (int j = 0; j < matrix->cols; ++j) {
-            cout << matrix->matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
 }
