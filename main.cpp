184dcf4c7cba6ef61f652c76739e39deb21121f1
Jason Vo <102493073+voxuanthang203@users.noreply.github.com>
2023-07-16 19:43:05 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,27 @@
 
 using namespace std;
 
+#include "shortestPath.h"
 #include "handleInput.h"
 
 int main() {
-    string input = "input-small.txt";
+    string input = "my_input.txt";
 
     Matrix* matrix = createMatrix(input);
+
+    //edge case: empty matrix:
+    if (matrix->rows == 0 && matrix->cols == 0) {
+        cout << "Empty matrix" << endl;
+        return 0;
+    }
+
+    //call the shortestPath function to compute the optimal path
+    vector<vector<cell>> output = shortestPath(matrix);
+
+    //print out the result
+    for (int i = 0; i < matrix->cols; i++) {
+        cout << output[matrix->rows - 1][i].length << " " << output[matrix->rows - 1][i].start_cell << output[matrix->rows - 1][i].path << endl;
+    }
+
+    return 0;
 }
